adc111cec6e4425465415b1259f16f67e0ddabd6
Piyush2509 <piyush.jayaswal@gmail.com>
2018-07-19 11:55:29 -0400

    Unit test failure

--- src/main/java/guru/springframework/services/CategoryServiceImpl.java
+++ src/main/java/guru/springframework/services/CategoryServiceImpl.java
@@ -3,6 +3,8 @@ package guru.springframework.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Component;
+
 import guru.springframework.api.v1.mapper.CategoryMapper;
 import guru.springframework.api.v1.model.CategoryDTO;
 import guru.springframework.repositories.CategoryRepository;
@@ -10,6 +12,7 @@ import guru.springframework.repositories.CategoryRepository;
 /**
  * Created by piyush.b.kumar on Jul 18, 2018
  */
+@Component
 public class CategoryServiceImpl implements CategoryService {
 
 	private final CategoryMapper categoryMapper;
